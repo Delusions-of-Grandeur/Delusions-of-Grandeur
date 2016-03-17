58b63711ddb3a6696a8bab9545d27ecbcca7c69b
Dean Schaefer <Dean Schaefer>
2016-03-17 18:27:08 -0400

    adding spawning scene and scripts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SpawningFramework
+{
+    public class Enemy : MonoBehaviour
+    {
+
+        public float MaxHealth;
+        float health;
+        Vector3 pos;
+
+
+        [HideInInspector]
+        public bool alive;
+
+        void Update()
+        {
+        }
+
+        /// Called when this enemy has been spawned
+        void Start()
+        {
+            health = MaxHealth;
+            alive = true;
+            pos = transform.position;
+            
+            InvokeRepeating("move", 0.01f, .2f);
+
+        }
+
+        void move()
+        {
+            transform.Translate(0, 0.025f, 0);
+        }
+
+        /// Called when the player loses
+        public virtual void OnGameOver()
+        {
+        }
+
+        #region Taking Damage
+        /// Hurts the enemy and returns if it dies or not
+        public virtual bool Hurt(float damage)
+        {
+            if (!alive)
+                return false;//exit if dead
+
+            health -= damage;//hurt the enemy
+
+            if (health < 0.1)
+            {
+                alive = false;
+                return true;
+            }
+
+            return health < 0.1;//return if this will die
+        }
+
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject Enemy;
+    GameObject[] objs;
+    int top;
+
+    void Start()
+    {
+        objs = GameObject.FindGameObjectsWithTag("Spawn_Point");
+        top = objs.Length;
+        InvokeRepeating("CreateObject", 0.01f, 5.0f);
+    }
+
+    void CreateObject()
+    {
+        Instantiate(Enemy, objs[Random.Range(0,top)].GetComponent<Transform>().position, Quaternion.identity);
+    }
+
+    void Update () {
+    }
+}
