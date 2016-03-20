f0866dc31ca67d0e0839026a0856d4cf0a8819ff
Dean Schaefer <soccerace99@gmail.com>
2016-03-20 17:28:54 -0400

    added waves for spawning

---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnController : MonoBehaviour {
+
+    public GameObject Enemy;
+    GameObject[] objs;
+    int top;
+
+    public static int numAlive;
+    public int round;
+    bool active;
+
+	// Use this for initialization
+	void Start () {
+
+        objs = GameObject.FindGameObjectsWithTag("Spawn_Point");
+        top = objs.Length;
+
+        numAlive = 0;
+        round = 0;
+        active = false;
+
+        InvokeRepeating("waveController", 0.01f, 5);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void waveController()
+    {
+        if(!active)
+        {
+            Debug.Log("Active now = true");
+            active = true;
+            startWave();
+        }
+        else
+        {
+            if(numAlive == 0)
+            {
+                Debug.Log(" End round ");
+                active = false;
+                round++;
+            }
+        }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+
+    void startWave()
+    {
+        Debug.Log("Starting wave: " + round);
+        for(int i = 0; i < (2 + 2 * round); i++)
+        {
+            Invoke("Spawn", Random.Range(0, 2));
+        }
+    }
+
+    void Spawn()
+    {
+        SpawnEnemy();
+        SpawnEnemy();
+    }
+
+    public void SpawnEnemy()
+    {
+        Instantiate(Enemy, RandomCircle(objs[Random.Range(0, top + 1)].GetComponent<Transform>().position, 4), Quaternion.identity);
+        numAlive++;
+    }
+}
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,22 +3,20 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour {
 
-    public GameObject Enemy;
-    GameObject[] objs;
-    int top;
+    
 
     void Start()
     {
-        objs = GameObject.FindGameObjectsWithTag("Spawn_Point");
-        top = objs.Length;
-        InvokeRepeating("CreateObject", 0.01f, 5.0f);
+        
+//      InvokeRepeating("SpawnEnemy", 0.01f, 5.0f);
     }
 
-    void CreateObject()
+    void Update()
     {
-        Instantiate(Enemy, objs[Random.Range(0,top)].GetComponent<Transform>().position, Quaternion.identity);
     }
 
-    void Update () {
-    }
+   
+
+
+
 }
