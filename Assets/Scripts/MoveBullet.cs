217f7a004a1ea326cbb78ec09f1df708f790d1c0
Dean Schaefer <soccerace99@gmail.com>
2016-04-05 19:23:31 -0400

    added assets, terrain

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -7,11 +7,16 @@ public class MoveBullet : MonoBehaviour {
 
 	void Start ()	
  	{
-		Destroy(gameObject, 5f); //Delete the bullet after 5 seconds
-	}
-	
-	void Update ()	
+        Destroy(gameObject, 7f); //Delete the bullet after 7 seconds
+    }
+
+    void Update ()	
  	{
 		transform.Translate(0, 0, speed);
 	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject); //Delete the bullet 
+    }
 }
