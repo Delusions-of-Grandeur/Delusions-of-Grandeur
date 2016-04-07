237db01d92975996e2a99dd6a1c32fc00d0c6b67
Erian Vazquez <erian.vazquez@gmail.com>
2016-04-07 18:33:19 -0400

    added detection scripts

--- Assets/Scripts/TowerTargeting.cs
+++ Assets/Scripts/TowerTargeting.cs
@@ -10,12 +10,38 @@ public class TowerTargeting : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		SearchTarget ();
 	}
 
-	void OnTriggerEnter(Collider other)
+	void SearchTarget()
 	{
-		if(other.tag == "Cube_Tower_1")
-			Debug.Log("child goes ouch!");
+		GameObject newTarget = null;
+		float radius = transform.FindChild ("Range").transform.localScale.z;
+		print (radius);
+		Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+		Collider[] enemiesColliders = null;
+		for (int i = 0; i < hitColliders.Length; i++) {
+			if(hitColliders[i].tag == "Enemy" ){
+				print ("found enemy");
+			}
+		}
+
+//		for (var currentCollider : Collider in collidersInRange)
+//		{
+//			var currentEnemy : Enemy = currentCollider.GetComponent.<Enemy>();
+//
+//			if (newTarget == null)
+//			{
+//				newTarget = currentEnemy;
+//			}
+//
+//			else if (newTarget.health > currentEnemy.health)
+//			{
+//				newTarget = currentEnemy;
+//			}
+//		}
+//
+//		if (newTarget != null)
+//			currentTarget = newTarget.transform;
 	}
 }
